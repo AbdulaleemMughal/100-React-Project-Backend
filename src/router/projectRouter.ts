41e7885c371d3f77b3dc18aa64704fa2bc18aa87
Abdul Aleem Mughal <137313559+AbdulaleemMughal@users.noreply.github.com>
2025-12-06 14:50:03 +0500

    Routing changed

--- src/router/projectRouter.ts
+++ src/router/projectRouter.ts
@@ -13,6 +13,6 @@ projectRouter.post("/add-project", addProject);
 projectRouter.get("/get-project", getProjects);
 projectRouter.patch("/update-project/:id", updateProject);
 projectRouter.delete("/delete-project/:id", deleteProject);
-projectRouter.get("/delete-project/:id", getSingleProject);
+projectRouter.get("/get-project/:id", getSingleProject);
 
 export default projectRouter;
